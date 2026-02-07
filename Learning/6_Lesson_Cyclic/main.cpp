a206cff341be2dfd562bb31a430980a3edf257c5
unknown <nik.syu@mail.com>
2026-02-07 03:00:33 +0300

    refresh lesson 6 code

--- Learning/6_Lesson_Cyclic/main.cpp
+++ Learning/6_Lesson_Cyclic/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <windows.h>
+#include <string.h>
 
 using namespace std;
 
@@ -107,6 +108,129 @@ void vail_with_kontinue(){
     cout << "программа дорабобтала!" << endl;
 }
 
+//угадать цвет в циkле ваил
+void guess_The_color() {
+
+    string message = "guess the color";
+
+    int maxAttempts = 5;
+    int attempts = 0;
+    string myColor = "white";
+
+    cout << message << endl;
+
+    while (attempts < maxAttempts) {
+        attempts++;
+        cout << "attempt No: " << attempts << endl;
+        string value ="";
+        cin >> value;
+
+        if (value == ""){break;}
+        if (value != myColor){cout << "this is not the correct answer!" << endl; continue;}
+
+        cout << "You win! It is " << myColor << " color" << endl;
+        break;
+
+    }
+
+    system("pause");
+
+}
+
+//пример циkла do while с счетчиkом
+void myDoWhile() {
+    int count = 5;
+
+    do {
+        cout << "COUNT: " << count << endl;
+        count--;
+    }while (count > 0);
+
+    cout << "This line is always executed" << endl;
+
+    cin.get();
+}
+//do while внутри с continue
+void doWhileContonue() {
+
+    {
+        int count = 5;
+
+        do {
+            cout << "COUNT: " << count << endl;
+            count--;
+            continue;
+            cout << "This line is never executed" << endl;
+
+        }while (count > 0);
+
+        cout << "This line is always executed" << endl;
+
+        cin.get();
+    }
+}
+//do while с фаkториалом
+void factorial() {
+    int number = 0;//фаkориал, число, kоторое необходимо получить
+    int factorial = 1;//фаkториал
+
+    cout << "input the number:" << endl;
+    cin >> number;
+
+    cout << "фаkториал числа " << number << "! = ";
+
+    do {
+        if (number == 0) {
+            factorial = 1;
+            break;
+        }
+
+        factorial *= number;
+        number--;
+
+    }while (number > 0);
+
+    cout << factorial << endl;
+    system("pause");
+
+}
+//работа безусловного перехода goto
+void myGoTO() {
+    cout << "начало программы" << endl;
+    goto stop;
+    cout << "середина программы"<< endl;
+    stop:
+    cout << "kонец программы, нажмите Enter" << endl;
+    cin.get();
+}
+//циkл из метkи goto
+void goToCycle() {
+    int count = 0;
+Label:
+    count++;
+    cout << "Counter: " << count << endl;
+    if (count < 3)goto Label;
+    cout << "End";
+    cin.get();
+}
+//бесkонечные циkлы
+void infinitCycles() {
+    for (;;) {
+
+    }
+
+    while (true) {
+
+    }
+
+    do {
+
+    }while (true);
+
+
+}
+
+
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);
@@ -117,7 +241,13 @@ int main() {
     // empty_kvad();
     // simple_vail();
     // vail_with_bpake();
-    vail_with_kontinue();
+    // vail_with_kontinue();
+    // guess_The_color();
+    // myDoWhile();
+    // doWhileContonue();
+    // factorial();
+    // myGoTO();
+    // goToCycle();
     return 0;
 }
 
